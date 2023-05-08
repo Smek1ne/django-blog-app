c6bb5668fa614f32a7879c54b5537b5cf715d15c
Vlad Smek <smek1negithub@gmail.com>
2023-05-08 12:30:15 +0300

    Added: "Similar post recommendations in detail page"

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.core.paginator import (
-    Paginator,
-    EmptyPage,
-    PageNotAnInteger,
-)
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from environs import Env
@@ -20,6 +16,7 @@ env.read_env()  # read .env txt file, if it exists
 def post_list(request, tag_slug=None):
     posts = Post.published.all()
     tag = None
+
     if tag_slug:
         tag = get_object_or_404(Tag, slug=tag_slug)
         posts = posts.filter(tags__in=[tag])
@@ -34,25 +31,34 @@ def post_list(request, tag_slug=None):
     except EmptyPage:
         posts_to_show = paginator.page(paginator.num_pages)
 
-    return render(request,
-                  'blog/post/list.html',
-                  {'posts': posts_to_show,
-                   'tag': tag})
+    return render(request, 'blog/post/list.html',
+                  {'posts': posts_to_show, 'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post,
-                             slug=post,
+    post = get_object_or_404(Post, slug=post,
                              publish__year=year,
                              publish__month=month,
                              publish__day=day,
                              status=Post.Status.PUBLISHED)
+
     comments = post.comments.filter(active=True)
     form = CommentForm()
 
-    return render(request,
-                  'blog/post/detail.html',
-                  {'post': post, 'comments': comments, 'form': form})
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = (
+        Post.published
+        .filter(tags__in=post_tags_ids)
+        .exclude(id=post.id)
+        .annotate(same_tags=Count('tags' in post_tags_ids))
+        .order_by('-same_tags', '-publish')[:4]
+    )
+
+    return render(request, 'blog/post/detail.html',
+                  {'post': post,
+                   'comments': comments,
+                   'form': form,
+                   'similar_posts': similar_posts})
 
 
 class PostListView(ListView):
@@ -67,33 +73,28 @@ def post_share(request, post_id):
                              id=post_id,
                              status=Post.Status.PUBLISHED)
     sent = False
-
     if request.method == 'POST':
         form = EmailPostForm(request.POST)
 
         if form.is_valid():
             cd: dict = form.cleaned_data
-            post_url = request.build_absolute_uri(
-                post.get_absolute_url())
+            post_url = request.build_absolute_uri(post.get_absolute_url())
             subj = f"{cd['name']} recommends you read {post.title}"
             msg = f"Read {post.title} at {post_url}\n\n" \
                   f"{cd['name']} comments: {cd['comments']}"
-            send_mail(subj, msg, 'audaceuix@gmail.com', [cd['to']])
+            send_mail(subj, msg, cd['email'], [cd['to']])
             sent = True
-
     else:
         form = EmailPostForm()
 
-    return render(request, 'blog/post/share.html', {'post': post,
-                                                    'form': form,
-                                                    'sent': sent})
+    return render(request, 'blog/post/share.html',
+                  {'post': post, 'form': form, 'sent': sent})
 
 
 @require_POST
 def post_comment(request, post_id):
-    post = get_object_or_404(Post,
-                             id=post_id,
-                             status=Post.Status.PUBLISHED)
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+
     comment = None
     form = CommentForm(data=request.POST)
 
